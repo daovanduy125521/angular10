75280dd94379a2d0d6e8521cb22d2015c8742bbd
duy dao <daovanduy125521@gmail.com>
2025-05-03 08:53:28 +0700

    clear view container ref

--- angular10/src/app/service/view-container-ref/component-outlet/component-outlet.component.ts
+++ angular10/src/app/service/view-container-ref/component-outlet/component-outlet.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef } from "@angular/core";
 import { AlertComponent } from "../alert.component";
 
 @Component({
@@ -6,12 +6,24 @@ import { AlertComponent } from "../alert.component";
     template: `
       <button (click)="loadComponent()">Tải Component</button>
       <ng-container *ngComponentOutlet="currentComponent"></ng-container>
+      <button (click)="loadComponent2()">Tải Component 2</button>
+      <ng-container #container></ng-container>
+      <button (click)="removeComponent()">Xoá Component 2</button>
     `
 })
 export class ComponentOutletComponent {
+    @ViewChild('container', { read: ViewContainerRef }) containerRef!: ViewContainerRef;
     currentComponent: any = null;
+    constructor(private resolver: ComponentFactoryResolver) {}
 
     loadComponent() {
         this.currentComponent = AlertComponent;
+    }
+    loadComponent2() {
+        const factory = this.resolver.resolveComponentFactory(AlertComponent);
+        this.containerRef.createComponent(factory)
+    }
+    removeComponent() {
+        this.containerRef.clear()
     }
 }
